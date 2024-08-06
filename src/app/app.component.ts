83c53372d4c5bc38457d116346b3fc9c6a877981
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-08-06 09:28:06 -0300

    Added structural folders + header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { HeaderComponent } from './presentation/components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular-18-InvestmentApp-Demo';
